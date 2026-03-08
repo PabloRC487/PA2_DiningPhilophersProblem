a3833ade3c3d2e8f2eb9c7b5f9c65bca4c462c1c
Julia <julia.arpon@gmail.com>
2026-03-08 12:27:08 -0400

    Task 3: variable num of Phil using scanner

--- common/BaseThread.java
+++ common/BaseThread.java
@@ -3,22 +3,19 @@ package common;
 import java.util.Random;
 
 /**
- * Class BaseThread
- * Simply one customized base class for many of our own threads.
+ * Class BaseThread Simply one customized base class for many of our own
+ * threads.
  *
- * An attempt to maintain an automatic unique TID (thread ID)
- * among all the derivatives and allow setting your own if needed.
- * Plus some methods for the sync exercises.
+ * An attempt to maintain an automatic unique TID (thread ID) among all the
+ * derivatives and allow setting your own if needed. Plus some methods for the
+ * sync exercises.
  *
  *
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
-public class BaseThread extends Thread
-{
+public class BaseThread extends Thread {
 	/*
-	 * ------------
-	 * Data members
-	 * ------------
+	 * ------------ Data members ------------
 	 */
 
 	/**
@@ -37,27 +34,23 @@ public class BaseThread extends Thread
 	private static int siTurn = 1;
 
 	/*
-	 * ------------
-	 * Constructors
-	 * ------------
+	 * ------------ Constructors ------------
 	 */
 
 	/**
 	 * Default
 	 */
-	public BaseThread()
-	{
+	public BaseThread() {
 		setTID();
 	}
 
 	/**
 	 * Assigns name to the thread and places it to the specified group
 	 *
-	 * @param poGroup ThreadGroup to add this thread to
+	 * @param poGroup  ThreadGroup to add this thread to
 	 * @param pstrName A string indicating human-readable thread's name
 	 */
-	public BaseThread(ThreadGroup poGroup, String pstrName)
-	{
+	public BaseThread(ThreadGroup poGroup, String pstrName) {
 		super(poGroup, pstrName);
 		setTID();
 	}
@@ -65,64 +58,49 @@ public class BaseThread extends Thread
 	/**
 	 * Sets user-specified TID
 	 */
-	public BaseThread(final int piTID)
-	{
+	public BaseThread(final int piTID) {
 		this.iTID = piTID;
 	}
 
 	/**
 	 * Retrieves our TID
+	 * 
 	 * @return TID, integer
 	 */
-	public final int getTID()
-	{
+	public final int getTID() {
 		return this.iTID;
 	}
 
 	/**
 	 * Sets internal TID and updates next TID on contruction time, so it's private.
 	 */
-	private final void setTID()
-	{
+	private final void setTID() {
 		this.iTID = siNextTID++;
 	}
 
 	/**
-	 * Just a make up for the PHASE I to make it somewhat tangeable.
-	 * Must be atomic.
+	 * Just a make up for the PHASE I to make it somewhat tangeable. Must be atomic.
 	 */
-	protected synchronized void phase1()
-	{
+	protected synchronized void phase1() {
 		System.out.println(this.getClass().getName() + " thread [TID=" + this.iTID + "] starts PHASE I.");
 
-		System.out.println
-		(
-			"Some stats info in the PHASE I:\n" +
-			"    iTID = " + this.iTID +
-			", siNextTID = " + siNextTID +
-			", siTurn = " + siTurn +
-			".\n    Their \"checksum\": " + (siNextTID * 100 + this.iTID * 10 + siTurn)
-		);
+		System.out.println("Some stats info in the PHASE I:\n" + "    iTID = " + this.iTID + ", siNextTID = "
+				+ siNextTID + ", siTurn = " + siTurn + ".\n    Their \"checksum\": "
+				+ (siNextTID * 100 + this.iTID * 10 + siTurn));
 
 		System.out.println(this.getClass().getName() + " thread [TID=" + this.iTID + "] finishes PHASE I.");
 	}
 
 	/**
-	 * Just a make up for the PHASE II to make it somewhat tangeable.
-	 * Must be atomic.
+	 * Just a make up for the PHASE II to make it somewhat tangeable. Must be
+	 * atomic.
 	 */
-	protected synchronized void phase2()
-	{
+	protected synchronized void phase2() {
 		System.out.println(this.getClass().getName() + " thread [TID=" + this.iTID + "] starts PHASE II.");
 
-		System.out.println
-		(
-			"Some stats info in the PHASE II:\n" +
-			"    iTID = " + this.iTID +
-			", siNextTID = " + siNextTID +
-			", siTurn = " + siTurn +
-			".\n    Their \"checksum\": " + (siNextTID * 100 + this.iTID * 10 + siTurn)
-		);
+		System.out.println("Some stats info in the PHASE II:\n" + "    iTID = " + this.iTID + ", siNextTID = "
+				+ siNextTID + ", siTurn = " + siTurn + ".\n    Their \"checksum\": "
+				+ (siNextTID * 100 + this.iTID * 10 + siTurn));
 
 		System.out.println(this.getClass().getName() + " thread [TID=" + this.iTID + "] finishes PHASE II.");
 	}
@@ -130,20 +108,19 @@ public class BaseThread extends Thread
 	/**
 	 * Test-and-Set for the iTurn variable.
 	 *
-	 * Use to proceed to the phase II in the correct order.
-	 * Must be atomic.
+	 * Use to proceed to the phase II in the correct order. Must be atomic.
 	 *
-	 * @param pcIncreasingOrder true if TIDs are in increasing order; false otherwise
+	 * @param pcIncreasingOrder true if TIDs are in increasing order; false
+	 *                          otherwise
 	 *
-	 * @return Returns true if if the TID of currently running thread  matches the turn, 'false' otherwise
+	 * @return Returns true if if the TID of currently running thread matches the
+	 *         turn, 'false' otherwise
 	 */
-	public synchronized boolean turnTestAndSet(boolean pcIncreasingOrder)
-	{
+	public synchronized boolean turnTestAndSet(boolean pcIncreasingOrder) {
 		// test
-		if(siTurn == this.iTID)
-		{
+		if (siTurn == this.iTID) {
 			// set siTurn = siTurn +/- 1;
-			if(pcIncreasingOrder == true)
+			if (pcIncreasingOrder == true)
 				siTurn++;
 			else
 				siTurn--;
@@ -157,33 +134,29 @@ public class BaseThread extends Thread
 	/**
 	 * Always assumes the increasing order
 	 */
-	public synchronized boolean turnTestAndSet()
-	{
+	public synchronized boolean turnTestAndSet() {
 		return turnTestAndSet(true);
 	}
 
 	/**
-	 * Allows setting arbitratu turn value. Should be set only before
-	 * the threads are started
+	 * Allows setting arbitratu turn value. Should be set only before the threads
+	 * are started
 	 */
-	public static void setInitialTurn(int piTurn)
-	{
+	public static void setInitialTurn(int piTurn) {
 		siTurn = piTurn;
 	}
 
 	/**
 	 * Default ascending order
 	 */
-	public static void setInitialTurnAscending()
-	{
+	public static void setInitialTurnAscending() {
 		setInitialTurn(1);
 	}
 
 	/**
 	 * Descending order
 	 */
-	public static void setInitialTurnDescending()
-	{
+	public static void setInitialTurnDescending() {
 		setInitialTurn(siNextTID - 1);
 	}
 
